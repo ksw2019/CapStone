51c0775cc3f8da9e6ac84e1fe1b5bfb45499fa0c
Kimsungwook <kkds03264@gmail.com>
2024-04-17 02:18:40 +0900

    4/17

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.capstone_projects"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.capstone_projects"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
+    }
+
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.navercorp.nid:oauth:5.9.1") //네이버 로그인 api
+    implementation("com.naver.maps:map-sdk:3.18.0") //네이버 맵 api
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/src/main/java/com/example/capstone_projects/HosipitalMap.kt
@@ -0,0 +1,15 @@
+package com.example.capstone_projects
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.naver.maps.map.NaverMapSdk
+
+class HosipitalMap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hosipital_map)
+
+        NaverMapSdk.getInstance(this).client =
+            NaverMapSdk.NaverCloudPlatformClient("xsilryo4zj")
+    }
+}
